da61b414f825797c9fadf52b89ea4b5260887f9f
Marco Cilona <marco.cilona@gmail.com>
2018-12-23 20:17:36 +0100

    Added some properties to server component

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  serverID: number = 30;
+  serverStatus: string = "offline";
+
   constructor() { }
 
   ngOnInit() {
