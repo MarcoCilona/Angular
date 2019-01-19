edf53ef937e9c7804698917fe908367e31ba773e
Marco Cilona <marco.cilona@gmail.com>
2019-01-19 18:44:52 +0100

    Add pug loader to project.

--- my-first-app/src/app/test-component/test-component.component.ts
+++ my-first-app/src/app/test-component/test-component.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test-component',
-  templateUrl: './test-component.component.html',
+  templateUrl: './test-component.component.1.pug',
   styleUrls: ['./test-component.component.css']
 })
 export class TestComponentComponent implements OnInit {
