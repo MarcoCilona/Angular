d140413b9986d1b9156fcb1ba6bdafcaf030a66f
Marco Cilona <marco.cilona@gmail.com>
2019-02-03 13:03:56 +0100

    Created custom directive

--- shopping-list-app/src/app/common/commonModule.module.ts
+++ shopping-list-app/src/app/common/commonModule.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { DropDownDirective } from './drop-down.directive';
 
 @NgModule({
+    declarations: [
+        DropDownDirective
+    ],
     imports: [
         BrowserModule,
         FormsModule
     ],
     exports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        DropDownDirective
     ]
 })
 
